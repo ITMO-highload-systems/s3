495cd759c31e3e8d4737e1731a1c56e5a1cdf910
Ivanov Nikita <iwaa0303@mail.ru>
2024-11-20 19:34:22 +0300

    added fallback (#3)

--- src/main/kotlin/org/example/notions3/client/CoreClient.kt
+++ src/main/kotlin/org/example/notions3/client/CoreClient.kt
@@ -1,17 +1,28 @@
 package org.example.notions3.client
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import reactivefeign.spring.config.ReactiveFeignClient
+import org.example.notions3.client.feign.CoreFeignClient
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
 import reactor.core.publisher.Mono
+import javax.naming.ServiceUnavailableException
 
-@ReactiveFeignClient(
-    value = "notion"
-)
-interface CoreClient {
+@Service
+class CoreClient(
+    private val coreFeignClient: CoreFeignClient
+) {
+    companion object {
+        private val logger = LoggerFactory.getLogger(CoreClient::class.java)
+        private const val FALLBACK_MESSAGE = "Core Service is unavailable"
+    }
 
-    @GetMapping("/api/v1/paragraph/paragraphs/{paragraphId}/images/availability")
-    @CircuitBreaker(name = "default")
-    fun isPossibleAddImageToParagraph(@PathVariable paragraphId: String): Mono<Boolean>
+    @CircuitBreaker(name = "default", fallbackMethod = "isPossibleAddImageToParagraphFallback")
+    fun isPossibleAddImageToParagraph(paragraphId: String): Mono<Boolean> {
+        return coreFeignClient.isPossibleAddImageToParagraph(paragraphId)
+    }
+
+    fun isPossibleAddImageToParagraphFallback(paragraphId: String, ex: Throwable): Mono<Boolean> {
+        logger.error("Fallback for isPossibleAddImageToParagraph invoked due to: ${ex.message}")
+        throw ServiceUnavailableException(FALLBACK_MESSAGE)
+    }
 }
